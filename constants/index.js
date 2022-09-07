5858b613b86b528eded72ab871b69e45d0fb7a69
personal <alihayder19@gmail.com>
2022-09-08 04:15:10 +0600

    Initial Commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xe6A1A4a0Bc73502514173f0Dd3fbC65042157e10";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
